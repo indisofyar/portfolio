2ba57d8c9d8b066cfab485b579bb4afc618326d3
indisofyar <38954340+indisofyar@users.noreply.github.com>
2023-04-15 08:50:02 +0100

    Progressed portfolio

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+
+  <nav class="container">
+    <div class="col-12 nav-container">
+      <img src="images/logo-new.svg" />
+      <ul class="nav-items-container">
+        <li><router-link to="/">Home</router-link></li>
+        <li> <router-link to="/about">About</router-link></li>
+        <li><a href="contact.html">Contact</a></li>
+      </ul>
+    </div>
   </nav>
-  <router-view/>
+    
+   
+  <router-view />
+
+  <footer class="container" data-aos="fade-up" data-aos-duration="800">
+    <div class="row gx-5">
+      <div class="col-3">
+        <img class="img" src="images/logo-new.svg" />
+      </div>
+      <div class="col-9 d-flex justify-content-end">
+        <ul>
+          <li><a href="#">Project - MyPocketSkill</a></li>
+          <li><a href="#">Project - Placeholder</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+// @import "@/assets/scss/_global.scss";
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,21 @@
+const path = require('path');
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      sass: {
+        sassOptions: {
+          includePaths: [
+            path.resolve(__dirname, 'src/core/')
+          ],
+          indentedSyntax: true,
+        },
+        additionalData: '@import "~@/assets/scss/_global.scss"',
+      },
+    },
+  },
+  assetsDir: '@/assets/',
   publicPath: process.env.NODE_ENV === 'production'
     ? '/portfolio-2023/'
     : '/'
